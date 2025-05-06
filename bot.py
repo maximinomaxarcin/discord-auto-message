12101f5a9cb8e9a0ea5f65fe257c1158491545fd
Guspring <ikmalashshiddiq@gmail.com>
2025-05-06 16:46:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,6 +49,7 @@ token = input("Masukkan Token Bot Discord: ").strip()
 channel_ids_input = input("Masukkan ID Saluran Discord (pisahkan dengan koma): ").strip()
 pesan_input = input("Masukkan pesan yang ingin Anda kirim: ").strip()
 waktu_kirim = 86400  # 24 jam dalam detik
+waktu_jeda = 5  # Jeda 5 detik antara pengiriman pesan
 # Mengubah ID saluran menjadi integer
 channel_ids = [int(cid.strip()) for cid in channel_ids_input.split(",")]
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -67,5 +68,7 @@ while True:
             print(Fore.YELLOW + payload['content'])
         else:
             print(Fore.RED + f'Gagal mengirim pesan ke channel ID {channel_id}: {r.status_code}')
+        # Jeda selama 5 detik sebelum mengirim pesan ke saluran berikutnya
+        time.sleep(waktu_jeda)
     print(f"Menunggu selama {waktu_kirim} detik sebelum mengirim pesan lagi...")
     time.sleep(waktu_kirim)  # Tunggu selama 24 jam
